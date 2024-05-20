5506b4ea369d81fd593b5ca370602b400a8be47d
bahauddin fahad <bahauddin.fahad819@gmail.com>
2024-05-20 21:40:17 +0600

    added model

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+import {
+  TProduct,
+  TProductInventory,
+  TProductVariant,
+} from './product.interface';
+
+const productVariantSchema = new Schema<TProductVariant>({
+  type: [
+    {
+      type: { type: String, required: true },
+      value: { type: String, required: true },
+    },
+  ],
+});
+const productInventorySchema = new Schema<TProductInventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true, default: true },
+});
+const productSchema = new Schema<TProduct>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String },
+  tags: { type: [{ type: String, required: true }], required: true },
+  variants: { type: productVariantSchema, required: true },
+  inventory: { type: productInventorySchema, required: true },
+});
+
+export const ModelProduct = model<TProduct>('Product', productSchema);
